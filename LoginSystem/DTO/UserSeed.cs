45ab283308d25fb1de03ad9488ad39e7292da480
Kishor <Kishor@codebee.dk>
2024-06-07 11:24:30 +0545

    Feature-V Default value for Admin seed

---
+++ LoginSystem/DTO/UserSeed.cs
@@ -0,0 +1,27 @@
+﻿using LoginSystem.Model;
+using LoginSystem.Utility;
+using NETCore.Encrypt;
+using Org.BouncyCastle.Asn1.Ocsp;
+
+namespace LoginSystem.DTO
+{
+    public class UserSeed
+    {
+        public static  List<UserInfo> DefaultUser()
+        {
+            return new List<UserInfo>()
+            {
+                new UserInfo()
+                {
+                    UserId = new Guid("9d3a21ba-e76b-49e6-a24e-2cf9d1531994"),
+                    UserName = "Admin",
+                    Email = "nabinthekishor@gmail.com",
+                    IsActive = true,
+                    Password = EncryptProvider.Base64Encrypt("#Admin123"),
+                    ActivationCode = "678999",
+                    ExpirationDate = DateTime.Now.AddDays(1),
+                },
+            };
+        }
+    }
+}
---
+++ LoginSystem/Migrations/20240607053811_Default Admin User seed.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoginSystem.Migrations
+{
+    public partial class DefaultAdminUserseed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "UserInfos",
+                columns: new[] { "UserId", "ActivationCode", "Email", "ExpirationDate", "IsActive", "Password", "UserName" },
+                values: new object[] { new Guid("9d3a21ba-e76b-49e6-a24e-2cf9d1531994"), "678999", "nabinthekishor@gmail.com", new DateTime(2024, 6, 8, 11, 23, 11, 456, DateTimeKind.Local).AddTicks(7711), true, "I0FkbWluMTIz", "Admin" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "UserInfos",
+                keyColumn: "UserId",
+                keyValue: new Guid("9d3a21ba-e76b-49e6-a24e-2cf9d1531994"));
+        }
+    }
+}
