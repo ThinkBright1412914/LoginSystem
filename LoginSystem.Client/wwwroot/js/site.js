6c83416adbc7070cc34c838c1d1c82f63581b332
ThinkBright1412914 <Kishor@codebee.dk>
2024-06-11 20:39:43 +0545

    Fix layout for landing page

--- LoginSystem.Client/wwwroot/js/site.js
+++ LoginSystem.Client/wwwroot/js/site.js
@@ -1,1 +1,15 @@
-﻿
+﻿$('#showPassword').click(function () {
+    if ('password' == $('#Password').attr('type')) {
+        $('#Password').prop('type', 'text');
+    } else {
+        $('#Password').prop('type', 'password');
+    }
+})
+
+$('#showPassword2').click(function () {
+    if ('password' == $('#ConfirmPassword').attr('type')) {
+        $('#ConfirmPassword').prop('type', 'text');
+    } else {
+        $('#ConfirmPassword').prop('type', 'password');
+    }
+})
