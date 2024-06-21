8c4f2f8449e2258c10e8567c04250a323235ab55
ThinkBright1412914 <Kishor@codebee.dk>
2024-06-21 06:59:28 +0545

    Added jquery function to avoid white space

--- LoginSystem.Client/wwwroot/js/site.js
+++ LoginSystem.Client/wwwroot/js/site.js
@@ -1,4 +1,13 @@
-﻿$('#showPassword').click(function () {
+﻿
+function AvoidSpace(event) {
+    var value = event ? event.which : window.event.keyCode;
+    if (value === 32) return false;
+}
+
+
+
+
+$('#showPassword').click(function () {
     if ('password' == $('#Password').attr('type')) {
         $('#Password').prop('type', 'text');
     } else {
