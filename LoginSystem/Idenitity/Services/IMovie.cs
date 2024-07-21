5371ff27e5165982c72ab06ab17f52ad285939fb
ThinkBright1412914 <nabinthekishor@gmail.com>
2024-07-21 20:23:09 +0545

    Api for movie

---
+++ LoginSystem/Idenitity/Services/IMovie.cs
@@ -0,0 +1,13 @@
+﻿using LoginSystem.ViewModel;
+
+namespace LoginSystem.Idenitity.Services
+{
+	public interface IMovie
+	{
+		Task<List<MovieDto>> GetMoviess();
+		Task<MovieDto> GetMovieById(int id);	
+		Task<MovieDto> Create(MovieDto request);
+		Task<MovieDto> Update(MovieDto request);
+		Task<MovieDto> Delete(int id);
+	}
+}
---
+++ LoginSystem/ViewModel/MovieDto.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace LoginSystem.ViewModel
+{
+	public class MovieDto
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string? Image { get; set; }
+		public DateTime ReleaseDate { get; set; }
+		public string? Duration { get; set; }
+		public int LanguageId { get; set; }
+		public int GenreId { get; set; }
+		public int IndustryId { get; set; }
+	}
+}
