99c2f13a1a31c50a362c1ab2f3e02671304e6bbd
ThinkBright1412914 <nabinthekishor@gmail.com>
2024-07-21 16:12:56 +0545

    Updated Carouse

---
+++ LoginSystem.Client/wwwroot/js/UI.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('#autowidth').lightSlider({
+        item : 3,
+        autoWidth: false,
+        auto :true,
+        loop: true,
+        pager: false,
+        onSliderLoad: function () {
+            $('#autowidth').removeClass('cs-hidden');
+        }
+    })
+})
