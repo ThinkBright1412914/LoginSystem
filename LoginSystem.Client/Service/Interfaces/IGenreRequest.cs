49c0b31d4500c9b4022a63bfe4d8c096978aa93f
Kishor <Kishor@codebee.dk>
2024-08-07 13:34:05 +0545

    Refactoring the code

--- LoginSystem.Client/Service/Interfaces/IGenreRequest.cs
+++ LoginSystem.Client/Service/Interfaces/IGenreRequest.cs
@@ -1,4 +1,7 @@
-﻿using LoginSystem.ViewModel;
+﻿using LoginSystem.Utility;
+using LoginSystem.ViewModel;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace LoginSystem.Client.Service.Interfaces
 {
@@ -8,4 +11,34 @@ namespace LoginSystem.Client.Service.Interfaces
         Task<GenreDto> Create(GenreDto model);
         Task<GenreDto> Delete(int Id);
     }
+
+	public class GenreRequest : IGenreRequest
+	{
+		private readonly IhttpService _httpService;
+
+		public GenreRequest(IhttpService httpService)
+		{
+			_httpService = httpService;
+		}
+
+		public async Task<List<GenreDto>> GetAll()
+		{
+			var (status, response) = await _httpService.GetAsync<List<GenreDto>>(ApiUri.GetGenres);
+			return response;
+		}
+
+		public async Task<GenreDto> Create(GenreDto model)
+		{
+			string json = JsonConvert.SerializeObject(model);
+			StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+			var (status, response) = await _httpService.PostAsync<GenreDto>(ApiUri.CreateGenre, content);
+			return response;
+		}
+
+		public async Task<GenreDto> Delete(int Id)
+		{
+			var (status, response) = await _httpService.DeleteAsync<GenreDto>(ApiUri.DeleteGenre + "?Id=" + Id);
+			return response;
+		}
+	}
 }
--- LoginSystem.Client/Service/Interfaces/IMovieRequest.cs
+++ LoginSystem.Client/Service/Interfaces/IMovieRequest.cs
@@ -1,4 +1,7 @@
-﻿using LoginSystem.ViewModel;
+﻿using LoginSystem.Utility;
+using LoginSystem.ViewModel;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace LoginSystem.Client.Service.Interfaces
 {
@@ -10,4 +13,49 @@ namespace LoginSystem.Client.Service.Interfaces
         Task<MovieDto> Update(MovieDto model);
         Task<MovieDto> Delete(int Id);
     }
+
+	public class MovieRequest : IMovieRequest
+	{
+		private readonly IhttpService _httpService;
+
+		public MovieRequest(IhttpService httpService)
+		{
+			_httpService = httpService;
+		}
+
+		public async Task<List<MovieDto>> GetMovies(string filterMovie)
+		{
+			var (status, response) = await _httpService.GetAsync<List<MovieDto>>(ApiUri.GetMovies + "?filterMovies=" + filterMovie);
+			return response;
+		}
+
+		public async Task<MovieDto> GetMovieById(int Id)
+		{
+			var (status, response) = await _httpService.GetAsync<MovieDto>(ApiUri.GetMovieById + "?Id=" + Id);
+			return response;
+		}
+
+		public async Task<MovieDto> Create(MovieDto model)
+		{
+			string json = JsonConvert.SerializeObject(model);
+			StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+			var (status, response) = await _httpService.PostAsync<MovieDto>(ApiUri.CreateMovie, content);
+			return response;
+		}
+
+		public async Task<MovieDto> Update(MovieDto model)
+		{
+			string json = JsonConvert.SerializeObject(model);
+			StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+			var (status, response) = await _httpService.PutAsync<MovieDto>(ApiUri.UpdateMovie, content);
+			return response;
+		}
+
+		public async Task<MovieDto> Delete(int Id)
+		{
+			var (status, response) = await _httpService.DeleteAsync<MovieDto>(ApiUri.DeleteMovie + "?Id=" + Id);
+			return response;
+		}
+
+	}
 }
