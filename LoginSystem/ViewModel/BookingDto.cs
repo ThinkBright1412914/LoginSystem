e842beaf22f1c6bdccc1c5e28716c9d4e845fd5f
ThinkBright1412914 <nabinthekishor@gmail.com>
2024-08-09 20:52:02 +0545

    For booking

---
+++ LoginSystem/ViewModel/BookingDto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace LoginSystem.ViewModel
+{
+	public class BookingDto
+	{
+		public int Id { get; set; }
+		public Guid UserId { get; set; }
+		public int ShowId { get; set; }
+
+		public int NoOfTicket { get; set; }
+		public IEnumerable<SelectListItem> ShowList { get; set; }
+
+		public string SeatDetails { get; set; }
+		public DateTime BookingDate { get;set; }
+
+		public Decimal TotalAmount { get; set; }
+	}
+}
