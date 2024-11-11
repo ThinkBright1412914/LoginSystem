df91c3a7fb6817a05a37a93a09223356abfe4b45
ThinkBright1412914 <nabinthekishor@gmail.com>
2024-11-11 20:45:22 +0545

    Fixed js bugs

--- LoginSystem.Client/wwwroot/js/Booking.js
+++ LoginSystem.Client/wwwroot/js/Booking.js
@@ -1,112 +1,117 @@
-﻿$(function () {
-    var date = new Date();
-    var currentDate = (date.getMonth() + 1).toString().padStart(2, '0') + '/' +
-        date.getDate().toString().padStart(2, '0') + '/' +
-        date.getFullYear();
+﻿$(document).ready(function () {
+    $(function () {
+        var date = new Date();
+        var currentDate = (date.getMonth() + 1).toString().padStart(2, '0') + '/' +
+            date.getDate().toString().padStart(2, '0') + '/' +
+            date.getFullYear();
 
-    var futureDate = new Date();
-    futureDate.setDate(futureDate.getDate() + 1);
-    var maxDate = (futureDate.getMonth() + 1).toString().padStart(2, '0') + '/' +
-        futureDate.getDate().toString().padStart(2, '0') + '/' +
-        futureDate.getFullYear();
+        var futureDate = new Date();
+        futureDate.setDate(futureDate.getDate() + 1);
+        var maxDate = (futureDate.getMonth() + 1).toString().padStart(2, '0') + '/' +
+            futureDate.getDate().toString().padStart(2, '0') + '/' +
+            futureDate.getFullYear();
 
-    $('#bookingDate').daterangepicker({
-        singleDatePicker: true,
-        showDropdowns: true,
-        "minDate": currentDate,
-        "maxDate": maxDate,
+        $('#bookingDate').daterangepicker({
+            singleDatePicker: true,
+            showDropdowns: true,
+            "minDate": currentDate,
+            "maxDate": maxDate,
+        });
+    })
+
+
+
+    $('#onShowChange').on('change', function () {
+        var showId = parseInt($(this).val());
+        $.ajax({
+            url: '/Booking/GetOptionByShowId',
+            data: { 'Id': showId },
+            dataType: 'Json',
+            type: 'Get',
+            success: function (result) {
+                $('#ticketPrice').val(result.ticketPrice);
+                populateUI(result.reserveSeats);
+            }
+        })
     });
+
+
+    const container = document.querySelector(".movie-seat-container");
+    const seats = document.querySelectorAll(".seat-row .seat:not(.occupied)");
+    const count = document.getElementById("count");
+    const total = document.getElementById("totalAmount");
+    const movieSelect = document.getElementById("movie");
+
+
+    // Save selected movie index and price
+    function setMovieData(movieIndex, moviePrice) {
+        localStorage.setItem("selectedMovieIndex", movieIndex);
+        localStorage.setItem("selectedMoviePrice", moviePrice);
+    }
+
+    // Update total and count
+    function updateSelectedCount() {
+        let ticketPrice = parseInt($('#ticketPrice').val());
+        const selectedSeats = document.querySelectorAll(".seat-row .seat.selected");
+
+        const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+        localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
+        const selectedSeatsCount = selectedSeats.length;
+
+        count.innerText = selectedSeatsCount;
+
+        document.getElementById("ticketCount").value = selectedSeatsCount;
+        document.getElementById("reservedSeats").value = JSON.stringify(seatsIndex);
+
+        const seat = [...selectedSeats].map(seat => seat.innerText);
+
+        document.getElementById("seat-details").value = seat.join(", ");
+        document.getElementById("totalAmount").value = selectedSeatsCount * ticketPrice;
+
+    }
+
+    // Get data from localstorage and populate UI
+    function populateUI(value) {
+        seats.forEach((seat) => {
+            seat.classList.remove("occupied");
+        });
+
+        if (value !== null) {
+            const selectedSeats = JSON.parse(value);
+
+            if (selectedSeats !== null && selectedSeats.length > 0) {
+                seats.forEach((seat, index) => {
+                    if (selectedSeats.indexOf(index) > -1) {
+                        seat.classList.add("occupied");
+                    }
+                });
+            }
+
+            const selectedMovieIndex = value;
+
+            if (selectedMovieIndex !== null) {
+                movieSelect.selectedIndex = selectedMovieIndex;
+            }
+        }
+    }
+
+ Seat click event
+    container.addEventListener("click", (e) => {
+        if (
+            e.target.classList.contains("seat") &&
+            !e.target.classList.contains("occupied")
+        ) {
+            e.target.classList.toggle("selected");
+
+            updateSelectedCount();
+        }
+    });
+
+    // Initial count and total set
+    updateSelectedCount();
 })
 
 
 
-$('#onShowChange').on('change', function () {
-    var showId = parseInt($(this).val());
-    $.ajax({
-        url: '/Booking/GetOptionByShowId',
-        data: { 'Id': showId },
-        dataType: 'Json',
-        type: 'Get',
-        success: function (result) {
-            $('#ticketPrice').val(result.ticketPrice);
-            populateUI(result.reserveSeats);
-        }
-    })
-});
-
-
-const container = document.querySelector(".movie-seat-container");
-const seats = document.querySelectorAll(".seat-row .seat:not(.occupied)");
-const count = document.getElementById("count");
-const total = document.getElementById("totalAmount");
-const movieSelect = document.getElementById("movie");
-
-
-// Save selected movie index and price
-function setMovieData(movieIndex, moviePrice) {
-    localStorage.setItem("selectedMovieIndex", movieIndex);
-    localStorage.setItem("selectedMoviePrice", moviePrice);
-}
-
-// Update total and count
-function updateSelectedCount() {
-    let ticketPrice = parseInt($('#ticketPrice').val());
-    const selectedSeats = document.querySelectorAll(".seat-row .seat.selected");
-
-    const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
-
-    localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
-
-    const selectedSeatsCount = selectedSeats.length;
-
-    count.innerText = selectedSeatsCount;
-
-    document.getElementById("ticketCount").value = selectedSeatsCount;
-    document.getElementById("reservedSeats").value = JSON.stringify(seatsIndex);
-
-    const seat = [...selectedSeats].map(seat => seat.innerText);
-
-    document.getElementById("seat-details").value = seat.join(", ");
-    document.getElementById("totalAmount").value = selectedSeatsCount * ticketPrice;
-
-}
-
-// Get data from localstorage and populate UI
-function populateUI(value) {
-    seats.forEach((seat) => {
-        seat.classList.remove("occupied");
-    });
-
-    if (value !== null) {
-        const selectedSeats = JSON.parse(value);
-
-        if (selectedSeats !== null && selectedSeats.length > 0) {
-            seats.forEach((seat, index) => {
-                if (selectedSeats.indexOf(index) > -1) {
-                    seat.classList.add("occupied");
-                }
-            });
-        }
-
-        const selectedMovieIndex = value;
-
-        if (selectedMovieIndex !== null) {
-            movieSelect.selectedIndex = selectedMovieIndex;
-        }
-    }
-}
-
-// Seat click event
-container.addEventListener("click", (e) => {
-    if (
-        e.target.classList.contains("seat") &&
-        !e.target.classList.contains("occupied")
-    ) {
-        e.target.classList.toggle("selected");
-
-        updateSelectedCount();
-    }
-});
-
-// Initial count and total set
-updateSelectedCount();
